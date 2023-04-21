e9a8057827758d2a6cb5a6a37eead89ff0ee103c
Mikael Simberg <simberg@cscs.ch>
2023-04-21 16:18:15 +0200

    Force pika@0.14.0

--- spack/packages/dla-future/package.py
+++ spack/packages/dla-future/package.py
@@ -45,7 +45,7 @@ class DlaFuture(CMakePackage, CudaPackage, ROCmPackage):
     depends_on("umpire+rocm~shared", when="+rocm")
     depends_on("umpire@4.1.0:")
 
-    depends_on("pika@0.12:")
+    depends_on("pika@0.14.0")
     depends_on("pika-algorithms@0.1.1:")
     depends_on("pika +mpi")
     depends_on("pika +cuda", when="+cuda")
